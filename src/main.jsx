e8eb6146f4011e871b6d4ca2ef7f10f015775ae3
Gym Uruhimbi <gymuruhimbi@uruhimbis-iMac.local>
2025-07-08 13:53:08 +0200

    image added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import reactLogo from './react.png';
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+  
+  <div>
+    <img src={reactLogo} alt="React Logo" />
+    <h1>Hello</h1>
+
+  </div>
 )
