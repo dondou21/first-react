11488eea01a8328ea55d854f4a4ebabbd3426ca2
dondou21 <dondouabiyi@gmail.com>
2025-07-15 16:44:02 +0200

    entry structure done

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -1,11 +1,22 @@
 
 export default function Entry() {
     return (
-        <article>
-            <div>
-                <img className="main-image" src="https://scrimba.com/links/travel-journal-japan-image-url" alt="mount fuji" />
-                <h1>Hey</h1>
-            </div>
-        </article>
+            <main className="journal-entry" >
+                <div className="main-image-container" >
+                    <img className="main-image" src="https://scrimba.com/links/travel-journal-japan-image-url" alt="mount fuji" />
+                </div>
+                <div className="info-container" >
+                    <img className="marker"
+                    src="./marker.png" 
+                    alt="map marker icon" />
+                    <span className="country" >Japan</span>
+                    <h2 className="entry-title" >Mount Fuji</h2>
+                    <p className="trip-dates" >12 Jan, 2021 -24 Jan, 2021</p>
+                    <p className="entry-text" >Mount Fuji is the tallest mountain in Japan, standing 
+                        at 3,776 mters (12,389 feet). Mount Fuji is the single most popular 
+                        tourist site in Japan, for both Japanese and foreign tourists
+                    </p>
+                </div>
+            </main>
     )
 }
