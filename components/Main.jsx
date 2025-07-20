3528bdb7988a9e2f389f5c0c982e61935d2b946f
dondou21 <dondouabiyi@gmail.com>
2025-07-20 17:34:41 +0200

    event handdling

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,14 +1,33 @@
+const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+
+const ingredientLisstItems = ingredients.map(ingredient => (
+    <li key={ingredient}>{ingredient}</li>
+))
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const newIngredient = formData.get("ingredient")
+    ingredients.push(newIngredient)
+    console.log(ingredients)
+}
+
+console.log(ingredients)
 export default function Main() {
     return (
         <main>
-            <form className="add-ingredient-form" >
+            <form onSubmit={handleSubmit} className="add-ingredient-form" >
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button type="button">Add ingredient</button>
             </form>
+            <ul>
+                {ingredientLisstItems}
+            </ul>
         </main>
     )
 }
